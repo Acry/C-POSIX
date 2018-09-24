337fea0d3fc8ee73f4df3bc738a7762a3cbeabd2
Carsten Holtkamp <carsten-holtkamp@t-online.de>
2018-09-24 21:40:16 +0200

    small changes

--- stat.c
+++ stat.c
@@ -1,29 +1,21 @@
+/* stat.c
+ * use of fstat
+ */
+
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
 #include <stdlib.h>
-// #include <sys/types.h>
 #include <sys/stat.h>
-// #include <unistd.h>
 
-// POSIX USE OF FSTATAT(3P)/stat
-
-// int stat(const char *path, struct stat *buf);
-// const char *path	The file descriptor of the file that is being inquired.
-// struct stat *buf	A structure where data about the file will be stored.
-
-// return value	Returns a negative value on failure.
-
-int main(int argc, char **argv)
+int main(void)
 {
-// 	if(argc != 2)
-// 		return EXIT_FAILURE;
 
 	struct stat fileStat;
 	
-// 	if(stat(argv[1],&fileStat) < 0)
 	if(stat("stat.c",&fileStat) < 0)
 		return EXIT_FAILURE;
 	
-// 	printf("%s summery:\n",argv[1]);
 	printf("%s summery:\n","stat.c");
 	printf("Size: \t\t\t%ld bytes\n",fileStat.st_size);
 	printf("Number of Links: \t%lu\n",fileStat.st_nlink);
